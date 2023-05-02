31fb227ae70b143dada6308a547f149e80044096
unknown <kov1606@gmail.com>
2023-05-02 23:01:51 +0300

    add random and data

--- assets/index.js
+++ assets/index.js
@@ -8,6 +8,20 @@ const button = document.querySelector(".form__button");
 const chatInfo = document.querySelector(".chat__info"); 
 const chatComment = document.querySelector(".chat__comment"); 
 
+// Создаем переменную для массива со стандартными аватарами пользователя
+const images = [
+    "assets/images/bee.png",
+    "assets/images/chick.png",
+    "assets/images/crab.png",
+    "assets/images/fox.png",
+    "assets/images/jellyfish.png",
+    "assets/images/koala.png",
+    "assets/images/owl.png",
+    "assets/images/penguin.png",
+];
+
+let now = new Date();
+
 
 //Добавление комментария в чат при клике на кнопку
 button.addEventListener ('click', () => {
@@ -15,6 +29,15 @@ button.addEventListener ('click', () => {
     const userLink = userLinkElement.value; //извлекаю значение поля "Ссылка на аватар"
     const userComment = userCommentElement.value; //извлекаю значение поля "Комментарий"
 
+    // Рандом аватара
+    let userImage = "";
+    if (userLink =="") {
+        const randomImage = Math.floor(Math.random() * 9);
+        userImage = images[randomImage];
+    } else {
+        userImage = userLink;
+    }
+    
     // Функция для работы с регистром
     function normalizeName(name) {
         // Удаляем лишние пробелы в начале и конце имени
@@ -36,8 +59,9 @@ button.addEventListener ('click', () => {
     let correctName = normalizeName(userName); // присваиваем переменную корректному имени
 
     const censorUserComment = userComment.replace(/виагра/gi, "***").replace(/viagra/gi, "***").replace(/xxx/gi, "***").replace(/ххх/gi, "***"); // добавлена цензура в комментариях
-    chatInfo.innerHTML = `<img class="chat__image" src="${userLink}" alt="">
-    <div class="chat__username">${correctName}</div>`; //добавила разметку для добавления аватара и ФИО пользователя
+    chatInfo.innerHTML = `<img class="chat__image" src="${userImage}" alt="">
+    <div class="chat__username">${correctName}</div>
+    <div class="chat__date">${now}</div>`; //добавила разметку для добавления аватара и ФИО пользователя
     chatComment.innerHTML = `<p>${censorUserComment}</p>`; //добавила разметку для добавления комментария пользователя
     document.getElementById("form").reset(); //очистка полей формы после отправки
 });
